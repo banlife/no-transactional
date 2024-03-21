d5586ed8b2786314f81ce64477f8dbc1e0e3b2b1
조승현 <shjo@ban-life.com>
2024-03-21 13:42:51 +0900

    [PreReleased] JpaRepository로 관리하지 않는 테이블도 테스트 데이터 클리닝에 포함된다

--- src/main/java/banlife/DatabaseCleaner.java
+++ src/main/java/banlife/DatabaseCleaner.java
@@ -2,7 +2,7 @@ package banlife;
 
 import jakarta.persistence.EntityManager;
 import org.springframework.context.ApplicationContext;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.List;
@@ -10,21 +10,27 @@ import java.util.List;
 public abstract class DatabaseCleaner {
     public static void clear(ApplicationContext applicationContext) {
         var entityManager = applicationContext.getBean(EntityManager.class);
-        var repositories = applicationContext.getBeansOfType(JpaRepository.class).values().stream().toList();
+        var jdbcTemplate = applicationContext.getBean(JdbcTemplate.class);
         var transactionTemplate = applicationContext.getBean(TransactionTemplate.class);
 
         transactionTemplate.execute(status -> {
             entityManager.clear();
-            deleteAll(entityManager, repositories);
+            deleteAll(jdbcTemplate, entityManager);
             return null;
         });
     }
 
-    private static void deleteAll(EntityManager entityManager, List<JpaRepository> repositories) {
+    private static void deleteAll(JdbcTemplate jdbcTemplate, EntityManager entityManager) {
         entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
-        for (JpaRepository<?, ?> repository : repositories) {
-            repository.deleteAllInBatch();
+        for (String tableName : findDatabaseTableNames(jdbcTemplate)) {
+            entityManager.createNativeQuery("DELETE FROM %s".formatted(tableName)).executeUpdate();
         }
         entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
+    }
+
+    private static List<String> findDatabaseTableNames(JdbcTemplate jdbcTemplate) {
+        return jdbcTemplate
+                .query("SHOW TABLES", (rs, rowNum) -> rs.getString(1))
+                .stream().toList();
     }
 }
